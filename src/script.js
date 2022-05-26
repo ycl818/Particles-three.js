cf42177676a62f437d501dbf5e6ccb1cad78d9c9
yichien <kevinyichienlee@gmail.com>
2022-05-26 23:26:44 +0800

    added random colors mixing with #ff88cc

--- src/script.js
+++ src/script.js
@@ -21,23 +21,33 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 const particleTexture = textureLoader.load('/textures/particles/2.png')
 
+
+
 /**
  * Particles
  */
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry()
-const count = 5000
+const count = 20000
 
 const positions = new Float32Array(count * 3) // xyz/xyz/ ....
+const colors = new Float32Array(count * 3)
 
 for(let i = 0; i < count * 3; i++) {
     positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
 }
 
 particlesGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3)
 )
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial()
@@ -49,6 +59,8 @@ particlesMaterial.alphaMap = particleTexture
 //particlesMaterial.alphaTest = 0.001
 //particlesMaterial.depthTest = false
 particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true // let the colors show
 
 
 // Points
